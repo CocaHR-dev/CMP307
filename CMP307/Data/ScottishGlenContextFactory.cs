7a0ef2cb1fc750cfb584b97960883cd77751c0e8
Oleksandr <oleksandr.chuchko@nure.ua>
2024-11-14 10:36:45 +0000

    Created the appsettings.json file that includes the connection string to the database. Also created context files for the Entity Framework implementation.

---
+++ CMP307/Data/ScottishGlenContextFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+
+namespace CMP307.Data
+{
+
+    public class ScottishGlenContextFactory : IDesignTimeDbContextFactory<ScottishGlenContext>
+    {
+        public ScottishGlenContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ScottishGlenContext>();
+
+            try
+            {
+                // Build configuration
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("Properties/appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("ScottishGlenDb");
+
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., log the error)
+                Console.WriteLine($"An error occurred while creating the DbContext: {ex.Message}");
+                throw;
+            }
+
+            return new ScottishGlenContext(optionsBuilder.Options);
+        }
+    }
+}
