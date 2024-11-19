dba94c9fa20f8b50344c286d35efa5f9592af604
Oleksandr <oleksandr.chuchko@nure.ua>
2024-11-19 19:47:25 +0000

    Fixed a visual bug where when a button should not be generated in the grid, placeholder text would be created in its place. Also refactored the code a bit.

--- CMP307/Template Selectors/MainWindowButtonTemplateSelectors.cs
+++ CMP307/Template Selectors/MainWindowButtonTemplateSelectors.cs
@@ -17,27 +17,24 @@ public class AddEditButtonTemplateSelector : DataTemplateSelector
 
     public override DataTemplate SelectTemplate(object item, DependencyObject container)
     {
-        var element = container as FrameworkElement;
-        if (item is Employee employee)
+        if (container is FrameworkElement element)
         {
-            if (_context.Entry(employee).State == EntityState.Detached)
-                return element.FindResource("AddButtonTemplate") as DataTemplate;
-            else
-                return element.FindResource("EditButtonTemplate") as DataTemplate;
-        }
-        else if (item is Hardware hardware)
-        {
-            if (_context.Entry(hardware).State == EntityState.Detached)
-                return element.FindResource("AddButtonTemplate") as DataTemplate;
-            else
-                return element.FindResource("EditButtonTemplate") as DataTemplate;
-        }
-        else if (item is Department department)
-        {
-            if (_context.Entry(department).State == EntityState.Detached)
-                return element.FindResource("AddButtonTemplate") as DataTemplate;
-            else
-                return element.FindResource("EditButtonTemplate") as DataTemplate;
+            if (item is Employee employee)
+            {
+                var templateKey = _context.Entry(employee).State == EntityState.Detached ? "AddButtonTemplate" : "EditButtonTemplate";
+                return element.FindResource(templateKey) as DataTemplate;
+            }
+            else if (item is Hardware hardware)
+            {
+                var templateKey = _context.Entry(hardware).State == EntityState.Detached ? "AddButtonTemplate" : "EditButtonTemplate";
+                return element.FindResource(templateKey) as DataTemplate;
+            }
+            else if (item is Department department)
+            {
+                var templateKey = _context.Entry(department).State == EntityState.Detached ? "AddButtonTemplate" : "EditButtonTemplate";
+                return element.FindResource(templateKey) as DataTemplate;
+            }
+            return element.FindResource("EmptyTemplate") as DataTemplate;
         }
         return null;
     }
@@ -54,21 +51,30 @@ public class DeleteButtonTemplateSelector : DataTemplateSelector
 
     public override DataTemplate SelectTemplate(object item, DependencyObject container)
     {
-        var element = container as FrameworkElement;
-        if (item is Employee employee)
+        if (container is FrameworkElement element)
         {
-            if (_context.Entry(employee).State != EntityState.Detached)
-                return element.FindResource("DeleteButtonTemplate") as DataTemplate;
-        }
-        else if (item is Hardware hardware)
-        {
-            if (_context.Entry(hardware).State != EntityState.Detached)
-                return element.FindResource("DeleteButtonTemplate") as DataTemplate;
-        }
-        else if (item is Department department)
-        {
-            if (_context.Entry(department).State != EntityState.Detached)
-                return element.FindResource("DeleteButtonTemplate") as DataTemplate;
+            if (item is Employee employee)
+            {
+                if (_context.Entry(employee).State != EntityState.Detached)
+                {
+                    return element.FindResource("DeleteButtonTemplate") as DataTemplate;
+                }
+            }
+            else if (item is Hardware hardware)
+            {
+                if (_context.Entry(hardware).State != EntityState.Detached)
+                {
+                    return element.FindResource("DeleteButtonTemplate") as DataTemplate;
+                }
+            }
+            else if (item is Department department)
+            {
+                if (_context.Entry(department).State != EntityState.Detached)
+                {
+                    return element.FindResource("DeleteButtonTemplate") as DataTemplate;
+                }
+            }
+            return element.FindResource("EmptyTemplate") as DataTemplate;
         }
         return null;
     }
